92e8eb683d4c42deb8acf58e284c62085e8a0ad1
Aziz <atamirzaev.aziz@mail.ru>
2022-08-20 07:34:34 +0600

    created comment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./index.scss";
 
 function App() {
+  // hello world
   return (
     <div className="App">
       {/* <button className="open-modal-btn">✨ Открыть окно</button>
