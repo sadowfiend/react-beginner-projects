834dadae83d01fd97fac5bd418aa37fa79924c83
Aziz <atamirzaev.aziz@mail.ru>
2022-08-20 07:22:50 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
 
 function App() {
   return (
     <div className="App">
       <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
+      <div className="overlay">
         <div className="modal">
           <svg height="200" viewBox="0 0 200 200" width="200">
             <title />
@@ -13,7 +13,7 @@ function App() {
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
